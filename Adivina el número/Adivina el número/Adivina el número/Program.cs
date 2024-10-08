1ece46c05d704327736383f36a6fd360c91fc491
arenteva <retehalo2@hotmail.com>
2024-10-07 20:30:02 -0500

    Ingresando la cantidad de jugadores que van a jugar

--- Adivina el número/Adivina el número/Adivina el número/Program.cs
+++ Adivina el número/Adivina el número/Adivina el número/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-       //
+        do
+        {
+            //Ingresando la cantidad de jugadores que van a jugar
+            int jugadores = 0;
+            while (jugadores < 2 || jugadores > 4) ;
+
+
+        }
+        while (true);
     }
 }
