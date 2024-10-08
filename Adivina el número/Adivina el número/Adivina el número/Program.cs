e78b3b779113ea1d4e6cb42cf797ba77a7d0db7a
arenteva <retehalo2@hotmail.com>
2024-10-07 20:36:06 -0500

    Generar numero aleatorio

--- Adivina el número/Adivina el número/Adivina el número/Program.cs
+++ Adivina el número/Adivina el número/Adivina el número/Program.cs
@@ -6,7 +6,26 @@
         {
             //Ingresando la cantidad de jugadores que van a jugar
             int jugadores = 0;
-            while (jugadores < 2 || jugadores > 4) ;
+            while (jugadores < 2 || jugadores > 4);
+            {
+                Console.Clear();
+                Console.WriteLine("\nBienvenido al juego 'Adivina el número'");
+                Console.Write("Ingrese cuantas personas van a jugar (2 a 4): ");
+                jugadores = Convert.ToInt32(Console.ReadLine());
+
+                //validamos si cumple el requisito para el minimo de jugadores
+
+                if (jugadores < 2 || jugadores > 4)
+                {
+                    Console.WriteLine("\n¡Número de jugadores no válido! Deben ser entre 2 y 4 jugadores.");
+                    Console.WriteLine("Presiona Enter para continuar...");
+                    Console.ReadLine();
+                }
+
+                //Generar numero aleatorio
+
+
+            }
 
 
         }
